6c2374190a497ac655a1dc4ed37feb434ab39628
AlmeidaFabioO <fabio.633@hotmail.com>
2020-07-03 18:26:24 -0300

    logo altered

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
 import './styles.css'
-import logo from './logo.png'
 import banner from './banner.png'
 
 export default function Header() {
     return (
         <header className="top">
             <div className="logo">
-                <img src={logo} alt=""/>
+                <h1>OnePieceCards</h1>
             </div>
 
             <div className="banner">
